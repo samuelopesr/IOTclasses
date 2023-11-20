dc484788d11fad3064f1ed84d8c1e953ba85c221
Samuel <cerrilhado@gmail.com>
2023-11-20 14:19:58 -0300

    Z

--- project/front-end/front-end/src/api/api.ts
+++ project/front-end/front-end/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export async function getRotaPadrao(){
     const data = await axios.get('http://localhost:3001/status')
-
+    console.log(data);
     return data.data;
 }
 
